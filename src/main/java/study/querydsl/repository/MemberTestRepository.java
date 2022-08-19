37bebe29ce55e5dba370ccd4aeb82e8e0e1e5d04
hyse <gustmd4296@hanmail.net>
2022-08-19 11:07:11 +0900

    8.19(금)

---
+++ src/main/java/study/querydsl/repository/MemberTestRepository.java
@@ -0,0 +1,29 @@
+package study.querydsl.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
+import study.querydsl.repository.support.Querydsl4RepositorySupport;
+
+import java.util.List;
+
+import static study.querydsl.entity.QMember.*;
+
+public class MemberTestRepository extends Querydsl4RepositorySupport {
+    public MemberTestRepository(Class<?> domainClass) {
+        super(Member.class);
+    }
+
+    public List<Member> basicSelect() {
+        return select(member)
+                .from(member)
+                .fetch();
+    }
+
+    public List<Member> basicSelectFrom() {
+        return selectFrom(member)
+                .fetch();
+    }
+}
--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -18,6 +18,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberDto;
 import study.querydsl.dto.QMemberDto;
@@ -541,7 +542,7 @@ public class QuerydslBasicTest {
             builder.and(member.age.eq(ageCond));
         }
 
-        queryFactory
+        return queryFactory
                 .selectFrom(member)
                 .where(builder)
                 .fetch();
@@ -575,5 +576,65 @@ public class QuerydslBasicTest {
 
     private BooleanExpression allEq(String usernameCond, Integer ageCond) {
         return usernameEq(usernameCond).and(ageEq(ageCond));
+    }
+
+    @Test
+    @Commit
+    public void bulkUpdate() {
+
+        long count = queryFactory
+                .update(member)
+                .set(member.username, "비회원")
+                .where(member.age.lt(28))
+                .execute();
+
+        em.flush();
+        em.clear();
+    }
+
+    @Test
+    public void bulkAdd() {
+        long count = queryFactory
+                .update(member)
+                .set(member.age, member.age.multiply(2))
+                .execute();
+    }
+
+    @Test
+    public void bulkDelete() {
+        long count = queryFactory
+                .delete(member)
+                .where(member.age.gt(18))
+                .execute();
+    }
+
+    @Test
+    public void sqlFunction() {
+        List<String> result = queryFactory
+                .select(Expressions.stringTemplate(
+                        "function('replace',{0},{1},{2})",
+                        member.username, "member","M"))
+                .from(member)
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+    }
+
+
+    @Test
+    public void sqlFunction2() {
+        List<String> result = queryFactory
+                .select(member.username)
+                .from(member)
+                //.where(member.username.eq()
+                        //Expressions.stringTemplate("function('lower', {0})", member.username)))
+                        .where(member.username.eq(member.username.lower()))
+                        .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
     }
 }
